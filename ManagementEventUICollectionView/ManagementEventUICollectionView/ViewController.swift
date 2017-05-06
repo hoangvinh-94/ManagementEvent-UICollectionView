251af5e762f817045a962b48857095625518e245
Team_VinhPhamPro <downloadofvinh@gmail.com>
2017-05-06 11:37:54 +0700

    Add files via upload

---
+++ ManagementEventUICollectionView/ManagementEventUICollectionView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ManagementEventUICollectionView
+//
+//  Created by Cntt08 on 5/6/17.
+//  Copyright © 2017 Cntt08. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
